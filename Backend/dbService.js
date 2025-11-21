be0ebf659bea3463e293ad7f000a3efa4f90f910
gouthama320 <gouthama320@gmail.com>
2025-11-20 22:05:56 -0500

    Removed redundant function

--- Backend/dbService.js
+++ Backend/dbService.js
@@ -731,27 +731,27 @@ async clientLoadRequests(username) {
     } catch(err) { throw err; }
 }
 
-async upsertQuote(requestId, responderId, quotePrice, start, end, note, status) {
-    try {
-        const result = await new Promise((resolve, reject) => {
-            const query = `
-                INSERT INTO Quotes (request_id, responder_id, quote_price, scheduled_start, scheduled_end, note, status)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-                ON DUPLICATE KEY UPDATE
-                    quote_price = VALUES(quote_price),
-                    scheduled_start = VALUES(scheduled_start),
-                    scheduled_end = VALUES(scheduled_end),
-                    note = VALUES(note),
-                    status = VALUES(status)
-            `;
-            connection.query(query, [requestId, responderId, quotePrice, start, end, note, status], (err, res) => {
-                if(err) reject(err); 
-                else resolve(res.insertId || requestId); // return ID
-            });
-        });
-        return result;
-    } catch(err) { throw err; }
-}
+// async upsertQuote(requestId, responderId, quotePrice, start, end, note, status) {
+//     try {
+//         const result = await new Promise((resolve, reject) => {
+//             const query = `
+//                 INSERT INTO Quotes (request_id, responder_id, quote_price, scheduled_start, scheduled_end, note, status)
+//                 VALUES (?, ?, ?, ?, ?, ?, ?)
+//                 ON DUPLICATE KEY UPDATE
+//                     quote_price = VALUES(quote_price),
+//                     scheduled_start = VALUES(scheduled_start),
+//                     scheduled_end = VALUES(scheduled_end),
+//                     note = VALUES(note),
+//                     status = VALUES(status)
+//             `;
+//             connection.query(query, [requestId, responderId, quotePrice, start, end, note, status], (err, res) => {
+//                 if(err) reject(err); 
+//                 else resolve(res.insertId || requestId); // return ID
+//             });
+//         });
+//         return result;
+//     } catch(err) { throw err; }
+// }
 
 async updateQuote(requestId, status, note = null) {
   try {
